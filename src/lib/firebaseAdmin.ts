cc969c7cf899cef3f6fc989cbd4ca94c9f4a82e0
TopiHdex <topiltzin.hernandez.mares@gmail.com>
2025-05-31 19:53:29 +0200

    chore(fb): setup firebase

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,12 +1,18 @@
-import { initializeApp, cert, getApps } from 'firebase-admin/app';
+import { initializeApp, getApps } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
-const serviceAccount = JSON.parse(import.meta.env.FIREBASE_ADMIN_KEY as string);
+const firebaseConfig = {
+    apiKey: import.meta.env.API_KEY as string,
+    authDomain: import.meta.env.AUTH_DOMAIN as string,
+    projectId: import.meta.env.PROJECT_ID as string,
+    storageBucket: import.meta.env.STORAGE_BUCKET as string,
+    messagingSenderId: import.meta.env.MESSAGING_SENDER_ID as string,
+    appId: import.meta.env.APP_ID as string,
+    measurementId: import.meta.env.MEASUREMENT_ID as string
+};
 
 if (!getApps().length) {
-    initializeApp({
-        credential: cert(serviceAccount),
-    });
+    initializeApp(firebaseConfig);
 }
 
 export const db = getFirestore();
