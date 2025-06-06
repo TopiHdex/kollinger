e36fab2c8c4645c43ddf21e0843cb1a1c9277e85
TopiHdex <topiltzin.hernandez.mares@gmail.com>
2025-06-06 19:56:12 +0200

    chore(vercel): enable edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
-    adapter: vercel({}),
+    adapter: vercel({
+        edgeMiddleware: true,
+    }),
     integrations: [react()],
     image: {
         remotePatterns: [{ protocol: "https" }],
