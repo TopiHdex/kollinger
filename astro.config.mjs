e75bec825e98c8e5f64c6c5efb089f4e649f8436
TopiHdex <topiltzin.hernandez.mares@gmail.com>
2025-06-06 19:49:46 +0200

    chore(vercel): add addapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'static',
+    adapter: vercel({}),
     integrations: [react()],
     image: {
         remotePatterns: [{ protocol: "https" }],
